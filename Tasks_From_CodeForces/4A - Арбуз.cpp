798f9e258d49bed2cb0c95899a1c6920918d13d2
RomSty1e <62506277+RomSty1e@users.noreply.github.com>
2020-08-07 03:56:54 +0400

    Update 4A - Арбуз.cpp

--- Tasks_From_CodeForces/4A - Арбуз.cpp
+++ Tasks_From_CodeForces/4A - Арбуз.cpp
@@ -3,26 +3,19 @@
 #include <cmath>
 #include <string>
 #include <algorithm>
-
+ 
 using namespace std;
-
+ 
+ 
 int main()
 {
-	int n, m, a;
-	cin >> n >> m >> a;
-
-	int square = n * m;
-
-	int tile = a * a;
-
-	int ans = 1;
-
-	while (tile * ans < square)
-	{
-		ans++;
-	}
-
-	cout << ans;
-
+	int w;
+	cin >> w;
+ 
+	if (w % 2 == 0 && w != 2)
+		cout << "YES";
+	else
+		cout << "NO";
+ 
 	//system("pause");
 }
