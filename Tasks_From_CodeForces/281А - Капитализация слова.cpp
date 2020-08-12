99e984e4c843bc9a96b3fb9f1233e2ccdba96107
RomSty1e <62506277+RomSty1e@users.noreply.github.com>
2020-08-12 13:03:08 +0400

    Add files via upload

---
+++ Tasks_From_CodeForces/281А - Капитализация слова.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+	string s;
+	cin >> s;
+ 
+	s[0] = toupper(s[0]);
+ 
+	cout << s;
+ 
+	//system("pause");
+}
