cd89d5e6020c3268808c4243b5e0c8a0bd1637ff
RomSty1e <62506277+RomSty1e@users.noreply.github.com>
2020-08-12 12:51:04 +0400

    Add files via upload

---
+++ Tasks_From_CodeForces/158A - Следующий раунд.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+ 
+	vector<int> values(n);
+ 
+	for (int i = 0; i < n; i++)
+		cin >> values[i];
+ 
+	int target = values[k - 1];
+	int ans = 0;
+ 
+	for (int i = 0; i < n; i++)
+	{
+		if (values[i] >= target && values[i] > 0)
+			ans++;
+		else
+			break;
+	}
+ 
+	cout << ans;
+ 
+	//system("pause");
+}
