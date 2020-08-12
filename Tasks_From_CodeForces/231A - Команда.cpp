2c690099910234449ef0701635ef3d253fcbe68a
RomSty1e <62506277+RomSty1e@users.noreply.github.com>
2020-08-12 12:49:16 +0400

    Add files via upload

---
+++ Tasks_From_CodeForces/231A - Команда.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+	int n;
+	cin >> n;
+ 
+	int ans = 0;
+ 
+	vector<vector<int>> val(n, vector<int>(3));
+ 
+	for (int i = 0; i < n; i++)
+	{
+		int sum = 0;
+		for (int j = 0; j < 3; j++)
+		{
+			cin >> val[i][j];
+			sum += val[i][j];
+		}
+ 
+		if (sum >= 2)
+			ans++;
+	}
+ 
+	cout << ans;
+ 
+	//system("pause");
+}
