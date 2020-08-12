0e94d340138c195813786c8873fabf51cdc563f3
RomSty1e <62506277+RomSty1e@users.noreply.github.com>
2020-08-12 13:00:31 +0400

    Add files via upload

---
+++ Tasks_From_CodeForces/339А - Математика спешит на помощь.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+	string s;
+	cin >> s;
+ 
+	sort(s.begin(), s.end());
+ 
+	for (int i = 0; i < s.length(); i++)
+		if (s[i] == '+')
+		{
+			s.erase(i, 1);
+			i--;
+		}
+ 
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (i != s.length() - 1) 
+		{
+			s.insert(i + 1, "+");
+			i++;
+		}
+	}
+ 
+	cout << s;
+ 
+	//system("pause");
+}
