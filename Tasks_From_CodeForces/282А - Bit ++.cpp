f739ca06496e4f977ec36008a94b57e175b54d29
RomSty1e <62506277+RomSty1e@users.noreply.github.com>
2020-08-12 12:54:15 +0400

    Add files via upload

---
+++ Tasks_From_CodeForces/282А - Bit ++.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+	int n;
+	cin >> n;
+ 
+	vector<string> sen(n);
+ 
+	for (int i = 0; i < n; i++)
+		cin >> sen[i];
+ 
+	int ans = 0;
+ 
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (sen[i][j] == '+')
+			{
+				ans = ans + 1;
+				j = sen[i].length();
+			}
+			else if (sen[i][j] == '-')
+			{
+				ans = ans - 1;
+				j = sen[i].length();
+			}
+		}
+	}
+ 
+	cout << ans;
+	
+ 
+	//system("pause");
+}
