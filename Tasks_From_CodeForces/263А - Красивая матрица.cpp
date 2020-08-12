8e150ce1004129eb9fd5cef141a7dde14af25dfd
RomSty1e <62506277+RomSty1e@users.noreply.github.com>
2020-08-12 12:58:51 +0400

    Add files via upload

---
+++ Tasks_From_CodeForces/263А - Красивая матрица.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+	vector<vector<int>> matrix(5, vector<int> (5));
+ 
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 5; j++)
+			cin >> matrix[i][j];
+ 
+	int ans;
+ 
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			if (matrix[i][j] == 1)
+			{
+				ans = abs(i - 2) + abs(j - 2);
+			}
+		}
+	}
+ 
+	cout << ans;
+ 
+	//system("pause");
+}
